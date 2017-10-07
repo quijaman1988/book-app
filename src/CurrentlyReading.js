80a20acc7c9f43e59ecc0c037e7cba281db4e567
quijaman1988 <quijaman1988@gmail.com>
2017-10-07 13:29:08 -0500

    First Commit. Created first two components (Header and CurrentlyReading)

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,12 @@
+import React ,{Component} from 'react'
+import './App.css'
+
+class CurrentlyReading extends Component {
+  render () {
+    return (
+      <h1>Working</h1>
+    )
+  }
+}
+
+export default CurrentlyReading
